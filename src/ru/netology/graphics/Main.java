4b912ea67eef277c1da9fb961b78ca87d5b51109
AntonuseJV <anton954@rambler.ru>
2023-03-24 16:27:58 +0300

    Initial commit

---
+++ src/ru/netology/graphics/Main.java
@@ -0,0 +1,25 @@
+package ru.netology.graphics;
+
+import ru.netology.graphics.image.ImageToUnicodeConverter;
+import ru.netology.graphics.image.TextGraphicsConverter;
+import ru.netology.graphics.server.GServer;
+
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        TextGraphicsConverter converter = new ImageToUnicodeConverter();
+        GServer server = new GServer(converter);
+
+        server.start();
+
+        // TextGraphicsConverter converter = new ImageToUnicodeConverter();
+        // converter.setMaxRatio(4);
+        // converter.setMaxWidth(300);
+        // converter.setMaxHeight(300);
+        // String url = "https://raw.githubusercontent.com/netology-code/java-diplom/main/pics/simple-test.png";
+        // String url = "https://i.ibb.co/6DYM05G/edu0.jpg";
+        // String imgTxt = converter.convert(url);
+        // System.out.println(imgTxt);
+    }
+}
